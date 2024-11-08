0e16aa188e96e1efa92c283ba57c52232bc7133c
Kobe Pane <kobepane@gmail.com>
2024-11-08 10:42:36 -0800

    Added tests

--- test/testing/test_bank.py
+++ test/testing/test_bank.py
@@ -1,11 +1,20 @@
 import pytest
-   from bank import Account
+from bank import Account
 
-   def test_initial_balance():
-       # TODO
+def test_initial_balance():
+    '''
+    Testing initial balance as well as owner name is correct upon creating a new instance of the Account class
+    '''
+    account = Account("Kobe Pane", 1000.0)
+    assert account.owner == "Kobe Pane"
+    assert account.balance == 1000.0
 
-   def test_deposit():
-       # TODO
+def test_deposit():
+    account = Account("Kobe Pane", 1000.0)
+    account.deposit(550.0)
+    assert account.balance == 1550.0
 
-   def test_withdraw():
-       # TODO
+def test_withdraw():
+    account = Account("Kobe Pane", 1000.0)
+    account.withdraw(999.0)
+    assert account.balance == 1.0
